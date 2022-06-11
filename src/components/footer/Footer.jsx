59f98835a05c28146247d0b18c9dbda6b9925a0f
vunguyen1412201 <vunguyenhoang2001@gmail.com>
2022-06-11 17:18:19 +0700

    upload1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,49 +1,42 @@
-import React from 'react'
-import './footer.scss'
-import {Link} from 'react-router-dom'
-import bg from '../../assets/footer-bg.jpg'
-import logo from '../../assets/cgvlogo.png';
+import React from "react";
+import "./footer.scss";
+import { Link } from "react-router-dom";
+import bg from "../../assets/footer-bg.jpg";
+import logo from "../../assets/cgvlogo.png";
 
 const Footer = () => {
   return (
-    <div className="footer" style ={{backgroundImage:`url(${bg})`}}>
-        <div className="footer__content container">
-            <div className="footer__content logo">
-                 <div className="logo">
-                        <Link to="/">
-                    <img src={logo} alt=""/>
-
-                        </Link>
-
-                 </div>
-            </div>
-            <div className="footer__content__menus">
-                <div className="footer__content__menu">
-                    <Link to="/">Home</Link>
-                    <Link to="/">Contact us</Link>
-                    <Link to="/">Term of services</Link>
-                    <Link to="/">About us</Link>
-
-                </div>
-                <div className="footer__content__menu">
-                    <Link to="/">Live</Link>
-                    <Link to="/">FAQ</Link>
-                    <Link to="/">Prenium</Link>
-                    <Link to="/">Pravacy policy </Link>
-
-                </div>
-                <div className="footer__content__menu">
-                    <Link to="/">You must watch</Link>
-                    <Link to="/">Recent release</Link>
-                    <Link to="/">Top IDMB</Link>
-             
-
-                </div>
-            </div>
-
+    <div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="footer__content container">
+        <div className="footer__content logo">
+          <div className="logo">
+            <Link to="/">
+              <img src={logo} alt="" />
+            </Link>
+          </div>
         </div>
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to="/">Home</Link>
+            <Link to="/">Contact us</Link>
+            <Link to="/">Term of services</Link>
+            <Link to="/">About us</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Prenium</Link>
+            <Link to="/">Pravacy policy </Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">You must watch</Link>
+            <Link to="/">Recent release</Link>
+            <Link to="/">Top IDMB</Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
